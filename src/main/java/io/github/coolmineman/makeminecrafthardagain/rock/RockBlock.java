635de759ac7316e35ca0b6553b1c6850a2ce13ba
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-06-27 20:24:07 -0500

    r o c k

--- src/main/java/io/github/coolmineman/makeminecrafthardagain/rock/RockBlock.java
+++ src/main/java/io/github/coolmineman/makeminecrafthardagain/rock/RockBlock.java
@@ -4,13 +4,18 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.state.StateManager.Builder;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class RockBlock extends Block {
     public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
@@ -29,5 +34,12 @@ public class RockBlock extends Block {
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return VoxelShapes.cuboid(2/16f, 0f, 2/16f, 14/16f, 1/16f, 14/16f);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand,
+            BlockHitResult hit) {
+        world.breakBlock(pos, true, player);
+        return ActionResult.CONSUME;
     }
 }
